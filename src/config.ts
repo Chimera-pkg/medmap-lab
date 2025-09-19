1aa2b403eeae9c29335bcde559b48086d19f6506
Chimera-pkg <adryanmagatsu88@gmail.com>
2025-09-19 15:30:28 +0700

    fix: port chainmark

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,4 @@
 export const API_URL = "http://122.11.173.11:23335/v1";
 // export const API_URL = "http://localhost:3335/v1";
-export const CHAINMARKX_BASE_URL =
-  import.meta.env.VITE_CHAINMARKX_BASE_URL ?? "http://localhost:5000";
+export const CHAINMARKX_BASE_URL = "https://backend.chainmarkx.cognidex.ai";
 export const CHAINMARKX_USER_ID = import.meta.env.VITE_CHAINMARKX_USER_ID ?? "";
